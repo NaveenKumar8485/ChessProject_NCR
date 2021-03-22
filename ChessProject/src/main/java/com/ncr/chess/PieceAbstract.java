4dc3b53497825301f3b78b1e9d9792de071c5c39
Naveen8485 <naveenkumar.pv94@gmail.com>
2021-03-22 10:57:24 +0530

    Added Abstract Class

---
+++ ChessProject/src/main/java/com/ncr/chess/PieceAbstract.java
@@ -0,0 +1,57 @@
+package com.ncr.chess;
+
+public abstract class PieceAbstract {
+
+    private ChessBoard chessBoard;
+    private int xCoordinate;
+    private int yCoordinate;
+    private PieceColor pieceColor;
+
+    public PieceAbstract(PieceColor pieceColor) {
+        this.pieceColor = pieceColor;
+    }
+
+    public ChessBoard getChessBoard() {
+        return chessBoard;
+    }
+
+    public void setChessBoard(ChessBoard chessBoard) {
+        this.chessBoard = chessBoard;
+    }
+
+    public int getXCoordinate() {
+        return xCoordinate;
+    }
+
+    public void setXCoordinate(int value) {
+        this.xCoordinate = value;
+    }
+
+    public int getYCoordinate() {
+        return yCoordinate;
+    }
+
+    public void setYCoordinate(int value) {
+        this.yCoordinate = value;
+    }
+
+    public PieceColor getPieceColor() {
+        return this.pieceColor;
+    }
+
+    private void setPieceColor(PieceColor value) {
+        pieceColor = value;
+    }
+
+    public abstract void move(MovementType movementType, int newX, int newY);
+
+    @Override
+    public String toString() {
+        return getCurrentPositionAsString();
+    }
+
+    protected String getCurrentPositionAsString() {
+        String eol = System.lineSeparator();
+        return String.format("Current X: {1}{0}Current Y: {2}{0}Piece Color: {3}", eol, xCoordinate, yCoordinate, pieceColor);
+    }
+}
